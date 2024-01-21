ce556676f4c40a336631de74a2d3f0f2db1a64d7
01124833532mo <mohammedhamdi726@gmail.com>
2024-01-22 00:38:26 +0200

    It is a large marketplace that displays products and facilitates the payment process

---
+++ services/couponService.js
@@ -0,0 +1,38 @@
+const asyncHandler = require('express-async-handler');
+const factory = require('./handlersFactory');
+const Coupon = require('../models/couponModel');
+const ApiError = require('../utils/apiError');
+
+// @desc    Get list of coupons
+// @route   GET /api/v1/coupons
+// @access  Private/Admin-Manager
+exports.getCoupons = factory.getAll(Coupon);
+
+// @desc    Get specific coupon by id
+// @route   GET /api/v1/coupons/:id
+// @access  Private/Admin-Manager
+exports.getCoupon = factory.getOne(Coupon);
+
+// @desc    Create coupon
+// @route   POST  /api/v1/coupons
+// @access  Private/Admin-Manager
+exports.createCoupon = factory.createOne(Coupon);
+
+// @desc    Update specific coupon
+// @route   PUT /api/v1/coupons/:id
+// @access  Private/Admin-Manager
+exports.updateCoupon = factory.updateOne(Coupon);
+
+// @desc    Delete specific coupon
+// @route   DELETE /api/v1/coupons/:id
+// @access  Private/Admin-Manager
+exports.deleteCoupon =   asyncHandler(async (req, res, next) => {
+    const { id } = req.params;
+    const document = await Coupon.findByIdAndDelete(id);
+
+    if (!document) {
+      return next(new ApiError(`No document for this id ${id}`, 404));
+    }
+
+    res.status(204).send();
+  });
